6c60e1c5cfffc7ac09a208384003334656e6aa5a
zhangchuqi1998 <55055026+zhangchuqi1998@users.noreply.github.com>
2022-03-14 11:18:45 -0500

    1.0.5 hello world interface

--- src/main/java/com/aaron/wiki/config/WikiApplication.java
+++ src/main/java/com/aaron/wiki/config/WikiApplication.java
@@ -1,11 +1,13 @@
-package com.aaron.wiki;
+package com.aaron.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan("com.aaron")
 @SpringBootApplication
 public class WikiApplication {
 
