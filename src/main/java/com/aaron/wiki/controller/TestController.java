40cb05df7a2c598037228550396db997bd08e8e8
zhangchuqi1998 <55055026+zhangchuqi1998@users.noreply.github.com>
2022-03-14 15:55:46 -0500

    2.0.2 mybatis, developed /test/list interface, query data from test

--- src/main/java/com/aaron/wiki/controller/TestController.java
+++ src/main/java/com/aaron/wiki/controller/TestController.java
@@ -1,9 +1,14 @@
 package com.aaron.wiki.controller;
 
+import com.aaron.wiki.domain.Test;
+import com.aaron.wiki.service.TestService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 public class TestController {
@@ -11,13 +16,17 @@ public class TestController {
     @Value("${test.hello:TEST}")
     private String testHello;
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello World" + testHello;
-    }
+    @Resource
+    private TestService testService;
 
     @PostMapping("/hello/post")
     public String helloPost(String name) {
         return "Hello!"+name;
+    }
+
+
+    @GetMapping("/test/list")
+    public List<Test> list() {
+        return testService.list();
     }
 }
---
+++ src/main/java/com/aaron/wiki/mapper/TestMapper.java
@@ -0,0 +1,10 @@
+package com.aaron.wiki.mapper;
+
+import com.aaron.wiki.domain.Test;
+
+import java.util.List;
+
+public interface TestMapper {
+
+    public List<Test> list();
+}
