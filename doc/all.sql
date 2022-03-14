bcf9700db32736c462cd55bef8fae3af4b57faf1
zhangchuqi1998 <55055026+zhangchuqi1998@users.noreply.github.com>
2022-03-14 16:19:51 -0500

    2.0.3 mybatis generator demo form

--- doc/all.sql
+++ doc/all.sql
@@ -4,3 +4,12 @@ create table `test` (
   `name` varchar(50) comment 'name',
   primary key (`id`)
 ) engine=innodb default charset=utf8mb4 comment='test';
+
+drop table if exists `demo`;
+create table `demo` (
+                        `id` bigint not null comment 'id',
+                        `name` varchar(50) comment 'name',
+                        primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='test';
+
+insert into `demo` (id, name) values (1, 'test');
