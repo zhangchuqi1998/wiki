e44bcffe51aa5119360a8f522e40eb0e8cf58e1b
zhangchuqi1998 <55055026+zhangchuqi1998@users.noreply.github.com>
2022-03-14 19:39:38 -0500

    2.0.4 add ebook table and consistent layer

---
+++ src/main/java/com/aaron/wiki/service/DemoService.java
@@ -0,0 +1,19 @@
+package com.aaron.wiki.service;
+
+import com.aaron.wiki.domain.Demo;
+import com.aaron.wiki.mapper.DemoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DemoService {
+
+    @Resource
+    private DemoMapper demoMapper;
+
+    public List<Demo> list() {
+        return demoMapper.selectByExample(null);
+    }
+}
