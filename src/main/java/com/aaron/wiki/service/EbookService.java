88969ea69394ae569a6b202dfce1bf4eee8aa26c
zhangchuqi1998 <55055026+zhangchuqi1998@users.noreply.github.com>
2022-03-15 20:34:49 -0500

    3.0.8 change home page

--- src/main/java/com/aaron/wiki/service/EbookService.java
+++ src/main/java/com/aaron/wiki/service/EbookService.java
@@ -7,6 +7,7 @@ import com.aaron.wiki.req.EbookReq;
 import com.aaron.wiki.resp.EbookResp;
 import com.aaron.wiki.utils.CopyUtil;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -20,7 +21,9 @@ public class EbookService {
     public List<EbookResp> list(EbookReq req) {
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-        criteria.andNameLike("%"+ req.getName() + "%");
+        if (!ObjectUtils.isEmpty(req.getName())) {
+            criteria.andNameLike("%"+ req.getName() + "%");
+        }
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
         List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
