3b7c8e1be157b56b511ac956da3a1af160ae078b
zhangchuqi1998 <55055026+zhangchuqi1998@users.noreply.github.com>
2022-03-17 18:31:34 -0500

    4.0.4 ebook category change

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -32,6 +32,9 @@
                 <template #cover="{ text: cover }">
                     <img v-if="cover" :src="cover" alt="avatar" />
                 </template>
+                <template v-slot:category="{ text, record }">
+                    <span>{{ getCategoryName(record.category1Id) }} / {{ getCategoryName(record.category2Id) }}</span>
+                </template>
                 <template v-slot:action="{ text, record }">
                     <a-space size="small">
                         <a-button type="primary" @click="edit(record)">
@@ -66,11 +69,12 @@
             <a-form-item label="名称">
                 <a-input v-model:value="ebook.name" />
             </a-form-item>
-            <a-form-item label="分类一">
-                <a-input v-model:value="ebook.category1Id" />
-            </a-form-item>
-            <a-form-item label="分类二">
-                <a-input v-model:value="ebook.category2Id" />
+            <a-form-item label="分类">
+                <a-cascader
+                        v-model:value="categoryIds"
+                        :field-names="{ label: 'name', value: 'id', children: 'children' }"
+                        :options="level1"
+                />
             </a-form-item>
             <a-form-item label="描述">
                 <a-input v-model:value="ebook.description" type="textarea" />
@@ -109,13 +113,8 @@
                     dataIndex: 'name'
                 },
                 {
-                    title: '分类一',
-                    key: 'category1Id',
-                    dataIndex: 'category1Id'
-                },
-                {
-                    title: '分类二',
-                    dataIndex: 'category2Id'
+                    title: '分类',
+                    slots: { customRender: 'category' }
                 },
                 {
                     title: '文档数',
@@ -141,6 +140,8 @@
              **/
             const handleQuery = (params: any) => {
                 loading.value = true;
+                // 如果不清空现有数据，则编辑保存重新加载数据后，再点编辑，则列表显示的还是编辑前的数据
+                ebooks.value = [];
                 axios.get("/ebook/list", {
                     params: {
                         page: params.page,
@@ -174,11 +175,17 @@
             };
 
             // -------- 表单 ---------
-            const ebook = ref({});
+            /**
+             * 数组，[100, 101]对应：前端开发 / Vue
+             */
+            const categoryIds = ref();
+            const ebook = ref();
             const modalVisible = ref(false);
             const modalLoading = ref(false);
             const handleModalOk = () => {
                 modalLoading.value = true;
+                ebook.value.category1Id = categoryIds.value[0];
+                ebook.value.category2Id = categoryIds.value[1];
                 axios.post("/ebook/save", ebook.value).then((response) => {
                     modalLoading.value = false;
                     const data = response.data; // data = commonResp
@@ -202,6 +209,7 @@
             const edit = (record: any) => {
                 modalVisible.value = true;
                 ebook.value = Tool.copy(record);
+                categoryIds.value = [ebook.value.category1Id, ebook.value.category2Id]
             };
 
             /**
@@ -225,7 +233,43 @@
                 });
             };
 
+            const level1 =  ref();
+            let categorys: any;
+            /**
+             * 查询所有分类
+             **/
+            const handleQueryCategory = () => {
+                loading.value = true;
+                axios.get("/category/all").then((response) => {
+                    loading.value = false;
+                    const data = response.data;
+                    if (data.success) {
+                        categorys = data.content;
+                        console.log("原始数组：", categorys);
+
+                        level1.value = [];
+                        level1.value = Tool.array2Tree(categorys, 0);
+                        console.log("树形结构：", level1.value);
+                    } else {
+                        message.error(data.message);
+                    }
+                });
+            };
+
+            const getCategoryName = (cid: number) => {
+                // console.log(cid)
+                let result = "";
+                categorys.forEach((item: any) => {
+                    if (item.id === cid) {
+                        // return item.name; // 注意，这里直接return不起作用
+                        result = item.name;
+                    }
+                });
+                return result;
+            };
+
             onMounted(() => {
+                handleQueryCategory();
                 handleQuery({
                     page: 1,
                     size: pagination.value.pageSize,
@@ -240,6 +284,7 @@
                 loading,
                 handleTableChange,
                 handleQuery,
+                getCategoryName,
 
                 edit,
                 add,
@@ -248,6 +293,8 @@
                 modalVisible,
                 modalLoading,
                 handleModalOk,
+                categoryIds,
+                level1,
 
                 handleDelete
             }
@@ -261,3 +308,4 @@
         height: 50px;
     }
 </style>
+
