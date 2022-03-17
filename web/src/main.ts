1c89d9b0931a0b58b137273a8c67a9464d381990
zhangchuqi1998 <55055026+zhangchuqi1998@users.noreply.github.com>
2022-03-17 05:45:01 -0500

    3.1.1 axios interceptor

--- web/src/main.ts
+++ web/src/main.ts
@@ -9,6 +9,24 @@ import axios from 'axios';
 
 axios.defaults.baseURL = process.env.VUE_APP_SERVER;
 
+/**
+ * axios拦截器
+ */
+axios.interceptors.request.use(function (config) {
+    console.log('parameter：', config);
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+axios.interceptors.response.use(function (response) {
+    console.log('return value：', response);
+    return response;
+}, error => {
+    console.log('return false：', error);
+    return Promise.reject(error);
+});
+
+
 const app = createApp(App);
 app.use(store).use(router).use(Antd).mount('#app');
 
