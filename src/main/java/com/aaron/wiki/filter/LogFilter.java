a28aca338ce3ef8747f698bcbcc829b882fe289f
zhangchuqi1998 <55055026+zhangchuqi1998@users.noreply.github.com>
2022-03-17 06:22:06 -0500

    3.1.3 aop print interface and return value

--- src/main/java/com/aaron/wiki/filter/LogFilter.java
+++ src/main/java/com/aaron/wiki/filter/LogFilter.java
@@ -1,32 +1,32 @@
-package com.aaron.wiki.filter;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-
-@Component
-public class LogFilter implements Filter {
-
-    private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-
-    }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        LOG.info("------------- LogFilter begin -------------");
-        LOG.info("request address: {} {}", request.getRequestURL().toString(), request.getMethod());
-        LOG.info("remote address: {}", request.getRemoteAddr());
-
-        long startTime = System.currentTimeMillis();
-        filterChain.doFilter(servletRequest, servletResponse);
-        LOG.info("------------- LogFilter end time：{} ms -------------", System.currentTimeMillis() - startTime);
-    }
-}
+//package com.aaron.wiki.filter;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.stereotype.Component;
+//
+//import javax.servlet.*;
+//import javax.servlet.http.HttpServletRequest;
+//import java.io.IOException;
+//
+//@Component
+//public class LogFilter implements Filter {
+//
+//    private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
+//
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException {
+//
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//        HttpServletRequest request = (HttpServletRequest) servletRequest;
+//        LOG.info("------------- LogFilter begin -------------");
+//        LOG.info("request address: {} {}", request.getRequestURL().toString(), request.getMethod());
+//        LOG.info("remote address: {}", request.getRemoteAddr());
+//
+//        long startTime = System.currentTimeMillis();
+//        filterChain.doFilter(servletRequest, servletResponse);
+//        LOG.info("------------- LogFilter end time：{} ms -------------", System.currentTimeMillis() - startTime);
+//    }
+//}
