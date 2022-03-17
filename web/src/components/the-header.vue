91d527c287aa41827f8f5b1e5ed15ac20a5b7a4e
zhangchuqi1998 <55055026+zhangchuqi1998@users.noreply.github.com>
2022-03-17 07:12:45 -0500

    3.1.4 new page

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -6,14 +6,17 @@
                 mode="horizontal"
                 :style="{ lineHeight: '64px' }"
         >
-            <a-menu-item key="1">
-                nav 1111
+            <a-menu-item key="/">
+                <router-link to="/">Homepage</router-link>
+
             </a-menu-item>
-            <a-menu-item key="2">
-                nav 2
+            <a-menu-item key="/admin/ebook">
+                <router-link to="/admin/ebook">Wiki management</router-link>
+
             </a-menu-item>
-            <a-menu-item key="3">
-                nav 3
+            <a-menu-item key="/about">
+                <router-link to="/about">About Me</router-link>
+
             </a-menu-item>
         </a-menu>
     </a-layout-header>
---
+++ web/src/views/admin/admin-ebook.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Ebook management</h1>
+  </div>
+</template>
