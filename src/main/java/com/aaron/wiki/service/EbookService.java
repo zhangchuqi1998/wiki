c2161e76d955671127e57199d546bbb76dd2d848
zhangchuqi1998 <55055026+zhangchuqi1998@users.noreply.github.com>
2022-03-17 11:27:22 -0500

    3.1.6 PageHelper plugins

--- src/main/java/com/aaron/wiki/service/EbookService.java
+++ src/main/java/com/aaron/wiki/service/EbookService.java
@@ -6,6 +6,10 @@ import com.aaron.wiki.mapper.EbookMapper;
 import com.aaron.wiki.req.EbookReq;
 import com.aaron.wiki.resp.EbookResp;
 import com.aaron.wiki.utils.CopyUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -14,6 +18,8 @@ import java.util.List;
 
 @Service
 public class EbookService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
 
     @Resource
     private EbookMapper ebookMapper;
@@ -24,9 +30,15 @@ public class EbookService {
         if (!ObjectUtils.isEmpty(req.getName())) {
             criteria.andNameLike("%"+ req.getName() + "%");
         }
+        PageHelper.startPage(1,3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
         List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("Total rows: {}", pageInfo.getTotal());;
+        LOG.info("Total pages: {}", pageInfo.getPages());;
+
 
         return list;
     }
